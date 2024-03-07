559d88ee4e1db3da973e6dcad6f50d1280c017de
Alejandro Piad-Morffis <apiad@apiad.net>
2024-03-07 09:24:02 -0500

    Update

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from dotenv import load_dotenv
 from mistralai.client import MistralClient
-from mistralai.models.chat_completion import ChatMessage, Function
+from mistralai.models.chat_completion import ChatMessage
 
 import os
 
@@ -10,6 +10,11 @@ st.set_page_config("Demo LLM", "🦜")
 load_dotenv()
 API_KEY = os.getenv("MISTRAL_API_KEY")
 
+
+if "history" not in st.session_state:
+    st.session_state.history = []
+
+
 @st.cache_resource
 def get_client() -> MistralClient:
     return MistralClient(api_key=API_KEY)
@@ -17,10 +22,22 @@ def get_client() -> MistralClient:
 client = get_client()
 
 def chat(client:MistralClient, msg):
-    messages = [ChatMessage(role="user", content=msg)]
+    messages = [] #ChatMessage(role=m['role'], content=m['content']) for m in st.session_state.history]
+    messages.append(ChatMessage(role="user", content=msg))
+
+    # print(messages, flush=True)
+
+    response = ""
 
     for chunk in client.chat_stream(messages, model="mistral-small"):
+        response += chunk.choices[0].delta.content
         yield chunk.choices[0].delta.content
+
+    st.session_state.history.append(dict(role="ai", content=response))
+
+for message in st.session_state.history:
+    with st.chat_message(message['role']):
+        st.write(message['content'])
 
 query = st.chat_input()
 
@@ -28,6 +45,7 @@ if not query:
     st.stop()
 
 with st.chat_message("user"):
+    st.session_state.history.append(dict(role="user", content=query))
     st.write(query)
 
 with st.chat_message("ai"):
